b47a3528fd1d84bdce4588739057702cc050db32
DeadCham7 <jisyun@ucdavis.edu>
2017-04-25 23:52:59 -0700

    Add files via upload
    add file

---
+++ weather.js
@@ -0,0 +1,11 @@
+
+
+/* called when new weather arrives */
+
+function callbackFunction(data) {
+	var pgh = document.getElementById("forecast");
+    	pgh.textContent = JSON.stringify(data);
+var Imeanwtf
+}
+
+
